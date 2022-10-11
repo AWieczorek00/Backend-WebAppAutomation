9d7f92dc6c809cad3d86e0a25ea0ffae01313249
AWieczorek00 <awieczorek0000@gmail.com>
2022-10-11 22:56:33 +0200

    init

---
+++ src/main/java/pl/backendbscthesis/BackendBScThesisApplication.java
@@ -0,0 +1,13 @@
+package pl.backendbscthesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBScThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBScThesisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/backendbscthesis/BackendBScThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.backendbscthesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBScThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
