43ba508f145667be61e022c23e80c7e5d1261003
AWieczorek00 <awieczorek0000@gmail.com>
2023-02-26 17:16:27 +0100

    test

--- src/main/java/pl/backendbscthesis/Configuration/WebSecurityConfiguration.java
+++ src/main/java/pl/backendbscthesis/Configuration/WebSecurityConfiguration.java
@@ -69,6 +69,7 @@ public class WebSecurityConfiguration  extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         http.addFilterBefore(
+
                 sessionFilter,
                 UsernamePasswordAuthenticationFilter.class
         );
