d6d20ff7934590411bb6ce11f538a35563a91b9e
awieczorek <awieczorek0000@gmail.com>
2024-12-19 21:11:10 +0100

    Test

--- src/test/java/pl/backendbscthesis/BackendBScThesisApplicationTests.java
+++ src/test/java/pl/backendbscthesis/BackendBScThesisApplicationTests.java
@@ -1,24 +1,24 @@
-package pl.backendbscthesis;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import pl.backendbscthesis.security.Jwt.JwtService;
-
-import static org.junit.Assert.assertNotNull;
-
-@SpringBootTest
-@TestPropertySource(locations = "classpath:/application-test.properties")
-class BackendBScThesisApplicationTests {
-
-	@Autowired
-	private JwtService jwtService;
-
-	@Test
-	public void contextLoads() {
-		Assertions.assertNotNull(jwtService);
-	}
-
-}
+//package pl.backendbscthesis;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.TestPropertySource;
+//import pl.backendbscthesis.security.Jwt.JwtService;
+//
+//import static org.junit.Assert.assertNotNull;
+//
+//@SpringBootTest
+//@TestPropertySource(locations = "classpath:/application-test.properties")
+//class BackendBScThesisApplicationTests {
+//
+//	@Autowired
+//	private JwtService jwtService;
+//
+//	@Test
+//	public void contextLoads() {
+//		Assertions.assertNotNull(jwtService);
+//	}
+//
+//}
